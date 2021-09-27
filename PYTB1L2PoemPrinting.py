f6011f606dfbad6bec2e78d488e114f0e3134636
Adnan <adnanrajeh2003@gmail.com>
2021-09-27 20:18:39 +0200

    play with python

---
+++ PYTB1L2PoemPrinting.py
@@ -0,0 +1,10 @@
+vers1 = "Ik heet Adnan, ik eet veel banan."
+vers2 = "Ik woon in Hoorn, daar ben ik niet geboren."
+vers3 = "Ik hou van canada en gamen en salada."
+vers4 = "Ik hou van arma, dit game uit Amerika"
+vers5 = "Ik wil graag leren programmeren, om later goed te verdienenen, en programmeren vind ik leuk."
+vers6 = "die waren mijn gedichten, wat zijn jullie gewichten? "
+print(vers1, vers2)
+print(vers3, vers4,)
+print(vers5)
+print(vers6)
