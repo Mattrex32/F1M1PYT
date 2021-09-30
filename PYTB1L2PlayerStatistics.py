4bd02c56cc447fb80e96d8510805364028dd5801
Adnan <adnanrajeh2003@gmail.com>
2021-09-30 12:02:36 +0200

    PYTB1L2PlayerStatistics

---
+++ PYTB1L2PlayerStatistics.py
@@ -0,0 +1,15 @@
+naam = "name:      Mattrex32.      "
+leeftijd = "age:    23."
+damge = "damge:     70.             "
+health = "Health:   100."
+wapon = "Wapon:     AK 47."
+score = "Score:     697.            "
+kills = "Kills:     16."
+nok = "Nok:       two times.      "
+time = "Time:       14.06.3425."
+
+print(naam + leeftijd)
+print(damge + health)
+print(wapon)
+print(score + kills)
+print(nok + time)
