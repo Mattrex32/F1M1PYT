2306101dfd2af721cffd20151ea314de792a5a50
Adnan <adnanrajeh2003@gmail.com>
2021-09-30 12:14:22 +0200

    PYTB1L2MyFirstOperations

---
+++ PYTB1L2MyFirstOperations.py
@@ -0,0 +1,11 @@
+nummer1 = 3
+nummer2 = 3
+nummer3 = 5
+nummer4 = 6
+nummer5 = 3
+
+print(nummer1 + nummer2)
+print(nummer2 - nummer1)
+print(nummer4 * nummer3)
+print(nummer4 / nummer5)
+print(nummer4 % nummer5)
